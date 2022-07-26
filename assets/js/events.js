a809951d78770dc1d30633dcb968c0642d0ec0f6
Jacob  Bauer <jacobbauer@Jacobs-MacBook-Air.local>
2022-07-26 16:35:46 -0500

    condensed images

---
+++ assets/js/events.js
@@ -0,0 +1,9 @@
+const createLoremIpsum = require('./helpers');
+require('bootstrap');
+const createEl = require('./domMethods');
+if (window.location.href.indexOf('event') > -1) {
+}
+
+// $(document).ready( function() {
+//     // DOM manipulation code specific to each page.
+//     })
